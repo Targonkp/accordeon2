4ceaa335d3f459bbb81a1885d64de0c902b06917
Targonkp <R122BASRHa>
2019-11-18 17:18:08 +0300

    Accordeon2 created

---
+++ app.js
@@ -0,0 +1,12 @@
+var panelItem = document.querySelectorAll('.panel-title'),
+    active = document.getElementsByClassName('panel-active');
+
+Array.from(panelItem).forEach(function(item) {
+    item.addEventListener('click', function(e) {
+        if (active.length > 0 && active[0] !== this) // если есть активный элемент, и это не тот по которому кликнули
+            active[0].classList.remove('panel-active'); // убрать класс panel-active
+
+        // изменить состояние класса panel-active на текущем элементе: добавить если не было, убрать если было.
+        this.classList.toggle('panel-active');
+    });
+});
